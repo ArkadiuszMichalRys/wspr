7433ea690e83fa68d545120b49fc544c2ac35a1c
Arkadiusz Rys <Arkadiusz.Michal.Rys@gmail.com>
2018-09-28 21:18:20 +0200

    Packet types for mumble protocol

---
+++ wspr/protocol/packet_type.py
@@ -0,0 +1,32 @@
+import enum
+
+
+# Mumble control messages types
+# http://mumble-protocol.readthedocs.io/en/latest/protocol_stack_tcp.html
+class PacketType(enum.Enum):
+    VERSION = 0
+    UDPTUNNEL = 1
+    AUTHENTICATE = 2
+    PING = 3
+    REJECT = 4
+    SERVERSYNC = 5
+    CHANNELREMOVE = 6
+    CHANNELSTATE = 7
+    USERREMOVE = 8
+    USERSTATE = 9
+    BANLIST = 10
+    TEXTMESSAGE = 11
+    PERMISSIONDENIED = 12
+    ACL = 13
+    QUERYUSERS = 14
+    CRYPTSETUP = 15
+    CONTEXTACTIONMODIFY = 16
+    CONTEXTACTION = 17
+    USERLIST = 18
+    VOICETARGET = 19
+    PERMISSIONQUERY = 20
+    CODECVERSION = 21
+    USERSTATS = 22
+    REQUESTBLOB = 23
+    SERVERCONFIG = 24
+    SUGGESTCONFIG = 25
