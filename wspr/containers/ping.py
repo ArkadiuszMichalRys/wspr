8a3c20228afb92f3339e4d81cd5e0a8d49abc18d
Arkadiusz Rys <Arkadiusz.Michal.Rys@gmail.com>
2018-10-02 04:58:51 +0200

    React to received ping

--- wspr/containers/ping.py
+++ wspr/containers/ping.py
@@ -42,6 +42,29 @@ class Ping:
         """
         return self.time_received - self.time_sent
 
+    def received(self) -> None:
+        """"""
+        self.time_received = self.__get_current_time_milliseconds()
+        travel_time = self.__get_travel_time()
+
+        # We'll need the old average to calculate the variance
+        previous_average = self.average
+        # Calculate average using previous average with one more data point
+        self.average = ((self.average * self.amount_of_data_points) + travel_time) / (self.amount_of_data_points + 1)
+
+        # Variance
+        try:
+            # The average of the squared differences from the mean
+            a = pow(previous_average - self.average, 2)
+            b = self.amount_of_data_points
+            c = pow(travel_time - self.average, 2)
+            self.variance += a + c / b
+        except ZeroDivisionError:
+            self._logger.debug("Divide by zero on ping statistics.")
+
+        # We now have one extra data point
+        self.amount_of_data_points += 1
+
     def is_needed(self) -> bool:
         """"""
         return self.__get_current_time_milliseconds() >= self.time_sent + self.delay_milliseconds
