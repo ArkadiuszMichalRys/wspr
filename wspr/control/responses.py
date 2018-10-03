3cd1f6440bfb65f752f161652039eec428fc27f1
Arkadiusz Rys <Arkadiusz.Michal.Rys@gmail.com>
2018-10-03 17:31:42 +0200

    Documentation for responses

--- wspr/control/responses.py
+++ wspr/control/responses.py
@@ -2,5 +2,8 @@
 
 
 class Response:
+    """A response from whisper notifying the user some event occurred."""
+
     def __init__(self, packet):
+        """Contains packet which triggered response."""
         self.packet = packet
