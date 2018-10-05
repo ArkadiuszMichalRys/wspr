00109b05140523a17a238e8eb393002f1d8d043b
Arkadiusz Rys <Arkadiusz.Michal.Rys@gmail.com>
2018-10-05 11:41:15 +0200

    Possible exceptions

--- wspr/exceptions/exceptions.py
+++ wspr/exceptions/exceptions.py
@@ -7,3 +7,33 @@ class WhisperException(Exception):
     def __init__(self):
         """No specific message given."""
         self.value = ""
+
+
+class CodecNotSupportedError(WhisperException):
+    """Thrown when receiving an audio packet from an unsupported codec."""
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
+class InvalidFormatError(WhisperException):
+    """Thrown when receiving a packet we can't decode."""
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
+class InvalidVarInt(WhisperException):
+    """Thrown when trying to decode an invalid variable integer."""
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
