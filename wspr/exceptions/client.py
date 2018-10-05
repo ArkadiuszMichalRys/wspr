8a2aa7d4ed78e4e7a057964d7b483065bdb870fa
Arkadiusz Rys <Arkadiusz.Michal.Rys@gmail.com>
2018-10-05 11:49:17 +0200

    More possible exceptions

---
+++ wspr/exceptions/client.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from wspr.exceptions.exceptions import WhisperException
+
+
+class UnknownEventError(WhisperException):
+    """Thrown when accessing an unknown event."""
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return 'Event "{}" does not exists.'.format(self.value)
+
+
+class UnknownChannelError(WhisperException):
+    """Thrown accessing an unknown channel."""
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return "Channel '{}' could not be found.".format(self.value)
+
+
+class InvalidSoundDataError(WhisperException):
+    """Thrown when trying to send an invalid audio pcm data packet."""
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
