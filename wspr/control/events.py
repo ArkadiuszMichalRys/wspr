6ca2788cf9efcfeae250c1b1f2cec4e7980afa78
Arkadiusz Rys <Arkadiusz.Michal.Rys@gmail.com>
2018-10-09 14:46:23 +0200

    Change terminology in events

--- wspr/control/events.py
+++ wspr/control/events.py
@@ -6,134 +6,134 @@ from wspr.protocol.mumble_pb2 import Version, Authenticate, TextMessage
 class Event:
     """A response from whisper notifying the user some event occurred."""
 
-    def __init__(self, packet):
-        """Contains packet which triggered response."""
-        self.packet = packet
+    def __init__(self, payload):
+        """Contains payload which triggered response."""
+        self.payload = payload
 
 
 class VersionReceivedEvent(Event):
     """"""
 
-    def __init__(self, packet):
+    def __init__(self, payload):
         """"""
-        # for field, value in packet.ListFields():
+        # for field, value in payload.ListFields():
         #     setattr(self, field.name, value)
-        super().__init__(packet)
+        super().__init__(payload)
 
-    def get_packet(self) -> Version:
+    def get_payload(self) -> Version:
         """"""
         # version
         # release
         # os
         # os_version
         version = Version()
-        version.ParseFromString(self.packet)
+        version.ParseFromString(self.payload)
         return version
 
     def __repr__(self) -> str:
         """"""
-        v = self.get_packet()
+        v = self.get_payload()
         return "{} - {} - {} - {}".format(v.version, v.release, v.os, v.os_version)
 
 
 class UDPTunnelReceivedEvent(Event):
     """"""
 
-    def __init__(self, packet):
+    def __init__(self, payload):
         """"""
-        super().__init__(packet)
+        super().__init__(payload)
 
-    def get_packet(self):
+    def get_payload(self):
         """"""
         pass
-        # self.packet = mumble_pb2.UDPTunnel().ParseFromString(packet)
+        # self.payload = mumble_pb2.UDPTunnel().ParseFromString(payload)
 
 
 class AuthenticateReceivedEvent(Event):
     """"""
 
-    def __init__(self, packet):
+    def __init__(self, payload):
         """"""
-        super().__init__(packet)
+        super().__init__(payload)
 
-    def get_packet(self):
+    def get_payload(self):
         """"""
         auth = Authenticate()
-        auth.ParseFromString(self.packet)
+        auth.ParseFromString(self.payload)
 
 
 class PingReceivedEvent(Event):
     """"""
 
-    def __init__(self, packet):
+    def __init__(self, payload):
         """"""
-        super().__init__(packet)
+        super().__init__(payload)
 
 
 class RejectReceivedEvent(Event):
     """"""
 
-    def __init__(self, packet):
+    def __init__(self, payload):
         """"""
-        super().__init__(packet)
+        super().__init__(payload)
 
 
 class ServerSyncReceivedEvent(Event):
     """"""
 
-    def __init__(self, packet):
+    def __init__(self, payload):
         """"""
-        super().__init__(packet)
+        super().__init__(payload)
 
 
 class ChannelRemoveReceivedEvent(Event):
     """"""
 
-    def __init__(self, packet):
+    def __init__(self, payload):
         """"""
-        super().__init__(packet)
+        super().__init__(payload)
 
 
 class ChannelStateReceivedEvent(Event):
     """"""
 
-    def __init__(self, packet):
+    def __init__(self, payload):
         """"""
-        super().__init__(packet)
+        super().__init__(payload)
 
 
 class UserRemoveReceivedEvent(Event):
     """"""
 
-    def __init__(self, packet):
+    def __init__(self, payload):
         """"""
-        super().__init__(packet)
+        super().__init__(payload)
 
 
 class UserStateReceivedEvent(Event):
     """"""
 
-    def __init__(self, packet):
+    def __init__(self, payload):
         """"""
-        super().__init__(packet)
+        super().__init__(payload)
 
 
 class BanListReceivedEvent(Event):
     """"""
 
-    def __init__(self, packet):
+    def __init__(self, payload):
         """"""
-        super().__init__(packet)
+        super().__init__(payload)
 
 
 class TextMessageReceivedEvent(Event):
     """"""
 
-    def __init__(self, packet):
+    def __init__(self, payload):
         """"""
-        super().__init__(packet)
+        super().__init__(payload)
 
-    def get_packet(self):
+    def get_payload(self):
         """"""
         # actor
         # session
@@ -141,125 +141,125 @@ class TextMessageReceivedEvent(Event):
         # tree_id
         # message
         message = TextMessage()
-        message.ParseFromString(self.packet)
+        message.ParseFromString(self.payload)
         return message
 
     def __repr__(self):
         """"""
-        m = self.get_packet()
+        m = self.get_payload()
         return f"{m.message}"
 
 
 class PermissionDeniedReceivedEvent(Event):
     """"""
 
-    def __init__(self, packet):
+    def __init__(self, payload):
         """"""
-        super().__init__(packet)
+        super().__init__(payload)
 
 
 class ACLReceivedEvent(Event):
     """"""
 
-    def __init__(self, packet):
+    def __init__(self, payload):
         """"""
-        super().__init__(packet)
+        super().__init__(payload)
 
 
 class QueryUsersReceivedEvent(Event):
     """"""
 
-    def __init__(self, packet):
+    def __init__(self, payload):
         """"""
-        super().__init__(packet)
+        super().__init__(payload)
 
 
 class CryptSetupReceivedEvent(Event):
     """"""
 
-    def __init__(self, packet):
+    def __init__(self, payload):
         """"""
-        super().__init__(packet)
+        super().__init__(payload)
 
 
 class ContextActionModifyReceivedEvent(Event):
     """"""
 
-    def __init__(self, packet):
+    def __init__(self, payload):
         """"""
-        super().__init__(packet)
+        super().__init__(payload)
 
 
 class ContextActionReceivedEvent(Event):
     """"""
 
-    def __init__(self, packet):
+    def __init__(self, payload):
         """"""
-        super().__init__(packet)
+        super().__init__(payload)
 
 
 class UserListReceivedEvent(Event):
     """"""
 
-    def __init__(self, packet):
+    def __init__(self, payload):
         """"""
-        super().__init__(packet)
+        super().__init__(payload)
 
 
 class VoiceTargetReceivedEvent(Event):
     """"""
 
-    def __init__(self, packet):
+    def __init__(self, payload):
         """"""
-        super().__init__(packet)
+        super().__init__(payload)
 
 
 class PermissionQueryReceivedEvent(Event):
     """"""
 
-    def __init__(self, packet):
+    def __init__(self, payload):
         """"""
-        super().__init__(packet)
+        super().__init__(payload)
 
 
 class CodecVersionReceivedEvent(Event):
     """"""
 
-    def __init__(self, packet):
+    def __init__(self, payload):
         """"""
-        super().__init__(packet)
+        super().__init__(payload)
 
 
 class UserStatsReceivedEvent(Event):
     """"""
 
-    def __init__(self, packet):
+    def __init__(self, payload):
         """"""
-        super().__init__(packet)
+        super().__init__(payload)
 
 
 class RequestBlobReceivedEvent(Event):
     """"""
 
-    def __init__(self, packet):
+    def __init__(self, payload):
         """"""
-        super().__init__(packet)
+        super().__init__(payload)
 
 
 class ServerConfigReceivedEvent(Event):
     """"""
 
-    def __init__(self, packet):
+    def __init__(self, payload):
         """"""
-        super().__init__(packet)
+        super().__init__(payload)
 
 
 class SuggestConfigReceivedEvent(Event):
     """"""
 
-    def __init__(self, packet):
+    def __init__(self, payload):
         """"""
-        super().__init__(packet)
+        super().__init__(payload)
 
 
 class FullTreeEvent(Event):
